2a95fdb71dc356febc203c69042fc1be5fdfd95a
Alex <hozaalexandru@gmail.com>
2026-04-22 13:33:44 +0300

    ready !

---
+++ TicketManager/TicketManager.Tests.Integration/Workflows/AuthAndBookingViewModelIntegrationTests.cs
@@ -0,0 +1,181 @@
+using FluentAssertions;
+using TicketManager.Domain;
+using TicketManager.Repository;
+using TicketManager.Service;
+using TicketManager.ViewModel;
+
+namespace TicketManager.Tests.Integration.Workflows;
+
+public class AuthAndBookingViewModelIntegrationTests : BaseIntegrationTest
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IFlightRepository _flightRepository;
+    private readonly ITicketRepository _ticketRepository;
+    private readonly IAddOnRepository _addOnRepository;
+    private readonly IMembershipRepository _membershipRepository;
+    private readonly AuthService _authService;
+    private readonly BookingService _bookingService;
+    private readonly PricingService _pricingService;
+    private readonly NavigationService _navigationService;
+
+    public AuthAndBookingViewModelIntegrationTests()
+    {
+        var dbFactory = new DatabaseConnectionFactory(GetTestConnectionString());
+        _membershipRepository = new MembershipRepository(dbFactory);
+        _userRepository = new UserRepository(dbFactory, _membershipRepository);
+        _flightRepository = new FlightRepository(dbFactory);
+        _ticketRepository = new TicketRepository(dbFactory);
+        _addOnRepository = new AddOnRepository(dbFactory);
+
+        _authService = new AuthService(_userRepository);
+        _bookingService = new BookingService(_ticketRepository, _addOnRepository);
+        _pricingService = new PricingService();
+        _navigationService = new NavigationService();
+    }
+
+    [Fact]
+    public void TestAuthViewModel_RegisterAndLoginFlow()
+    {
+        var authVM = new AuthViewModel(_authService, _navigationService);
+        string code = Guid.NewGuid().ToString().Substring(0, 4);
+        string email = $"vasile.mihai_{code}@gmail.com";
+        string password = "Parola@Vasile123";
+
+        authVM.IsLoginMode = false;
+        authVM.EmailText = email;
+        authVM.PhoneText = "0733445566";
+        authVM.UsernameText = $"VasileM_{code}";
+        authVM.PasswordText = password;
+
+        authVM.ActionCommand.Execute(null);
+        authVM.SuccessMessage.Should().Contain("Registration successful");
+
+        authVM.IsLoginMode = true;
+        authVM.EmailText = email;
+        authVM.PasswordText = password;
+        authVM.ErrorMessage = "";
+
+        authVM.ActionCommand.Execute(null);
+        authVM.IsAuthenticated.Should().BeTrue();
+        authVM.AuthenticatedUser.Should().NotBeNull();
+        authVM.AuthenticatedUser.Email.Should().Be(email);
+    }
+
+    [Fact]
+    public void TestAuthViewModel_LoginFails_WithInvalidPassword()
+    {
+        var authVM = new AuthViewModel(_authService, _navigationService);
+        string code = Guid.NewGuid().ToString().Substring(0, 4);
+        string email = $"georgeta.popescu_{code}@gmail.com";
+        string correctPassword = "Parola@Georgeta456";
+
+        _authService.Register(email, "0722556677", $"GeorgetaP_{code}", correctPassword);
+
+        authVM.IsLoginMode = true;
+        authVM.EmailText = email;
+        authVM.PasswordText = "WrongPassword123";
+
+        authVM.ActionCommand.Execute(null);
+        authVM.IsAuthenticated.Should().BeFalse();
+        authVM.ErrorMessage.Should().Contain("Invalid");
+    }
+
+    [Fact]
+    public void TestBookingViewModel_InitializeAndUpdatePrices()
+    {
+        var bookingVM = new BookingViewModel(_bookingService, _pricingService, _navigationService);
+
+        var user = new User { UserId = 1, Email = "test@gmail.com", Username = "test" };
+        var flight = new Flight
+        {
+            FlightId = GetFirstAvailableFlightId(),
+            Route = new Route { Capacity = 180, DepartureTime = DateTime.Now.AddDays(5), ArrivalTime = DateTime.Now.AddDays(5).AddHours(2) }
+        };
+
+        bookingVM.InitializeAsync(flight, user).Wait();
+
+        bookingVM.CurrentFlight.Should().Be(flight);
+        bookingVM.CurrentUser.Should().Be(user);
+        bookingVM.Passengers.Count.Should().Be(1);
+    }
+
+    [Fact]
+    public void TestBookingViewModel_AddPassengers_UpdatesState()
+    {
+        var bookingVM = new BookingViewModel(_bookingService, _pricingService, _navigationService);
+
+        var user = new User { UserId = 1, Email = "rares.ionescu@gmail.com", Username = "rares" };
+        var flight = new Flight
+        {
+            FlightId = GetFirstAvailableFlightId(),
+            Route = new Route { Capacity = 180, DepartureTime = DateTime.Now.AddDays(3), ArrivalTime = DateTime.Now.AddDays(3).AddHours(1) }
+        };
+
+        bookingVM.InitializeAsync(flight, user, 3).Wait();
+
+        bookingVM.Passengers.Count.Should().Be(3);
+        bookingVM.CanAddPassenger.Should().BeFalse();
+    }
+
+    [Fact]
+    public void TestBookingViewModel_RemovePassenger_UpdatesCapacity()
+    {
+        var bookingVM = new BookingViewModel(_bookingService, _pricingService, _navigationService);
+
+        var user = new User { UserId = 1, Email = "adrian.stefan@gmail.com", Username = "adrian" };
+        var flight = new Flight
+        {
+            FlightId = GetFirstAvailableFlightId(),
+            Route = new Route { Capacity = 180, DepartureTime = DateTime.Now.AddDays(2), ArrivalTime = DateTime.Now.AddDays(2).AddHours(2) }
+        };
+
+        bookingVM.InitializeAsync(flight, user, 2).Wait();
+        int passengerCountBefore = bookingVM.Passengers.Count;
+
+        var passengerToRemove = bookingVM.Passengers[0];
+        bookingVM.RemovePassengerCommand.Execute(passengerToRemove);
+
+        bookingVM.Passengers.Count.Should().Be(passengerCountBefore - 1);
+        bookingVM.CanAddPassenger.Should().BeTrue();
+    }
+
+    [Fact]
+    public void TestDashboardViewModel_LoadTickets_AfterBooking()
+    {
+        string code = Guid.NewGuid().ToString().Substring(0, 4);
+        string email = $"cosmin.tudor_{code}@gmail.com";
+        string password = "Parola@Cosmin789";
+
+        _authService.Register(email, "0733667788", $"CosminT_{code}", password);
+        var user = _authService.Login(email, password);
+
+        UserSession.CurrentUser = user;
+        var dashboardVM = new DashboardViewModel(
+            new DashboardService(_ticketRepository),
+            new CancellationService(_ticketRepository),
+            _navigationService
+        );
+
+        dashboardVM.LoadUserTickets();
+
+        dashboardVM.OnNavigatedTo().Should().BeTrue();
+    }
+
+    [Fact]
+    public void TestDashboardViewModel_FilterTickets_ByUpcoming()
+    {
+        var user = new User { UserId = 1, Email = "test@gmail.com", Username = "test" };
+        UserSession.CurrentUser = user;
+
+        var dashboardVM = new DashboardViewModel(
+            new DashboardService(_ticketRepository),
+            new CancellationService(_ticketRepository),
+            _navigationService
+        );
+
+        dashboardVM.SelectedTicketFilter = "Upcoming";
+
+        dashboardVM.MyTickets.Should().NotBeNull();
+    }
+}
+
---
+++ TicketManager/TicketManager.Tests.Unit/ViewModel/BookingViewModelTests.cs
@@ -0,0 +1,130 @@
+using FluentAssertions;
+using Moq;
+using System.Collections.ObjectModel;
+using TicketManager.Domain;
+using TicketManager.Service;
+using TicketManager.ViewModel;
+
+namespace TicketManager.Tests.Unit.ViewModel;
+
+public class BookingViewModelTests
+{
+    private readonly Mock<IBookingService> _mockBookingService;
+    private readonly Mock<IPricingService> _mockPricingService;
+    private readonly Mock<INavigationService> _mockNavigationService;
+    private readonly BookingViewModel _viewModel;
+
+    public BookingViewModelTests()
+    {
+        UserSession.CurrentUser = null;
+        UserSession.PendingBookingParameters = null;
+        _mockBookingService = new Mock<IBookingService>();
+        _mockPricingService = new Mock<IPricingService>();
+        _mockNavigationService = new Mock<INavigationService>();
+        _mockBookingService.Setup(s => s.CalculateMaxPassengers(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>())).Returns(5);
+        _viewModel = new BookingViewModel(_mockBookingService.Object, _mockPricingService.Object, _mockNavigationService.Object);
+    }
+
+    [Fact]
+    public void AddPassengerCommand_AddsPassengerRespectingCapacity()
+    {
+        _viewModel.MaxPassengers = 2;
+        _viewModel.Passengers.Clear();
+
+        _viewModel.AddPassengerCommand.Execute(null);
+        _viewModel.Passengers.Count.Should().Be(1);
+
+        _viewModel.AddPassengerCommand.Execute(null);
+        _viewModel.Passengers.Count.Should().Be(2);
+
+        _viewModel.AddPassengerCommand.Execute(null);
+        _viewModel.Passengers.Count.Should().Be(2);
+    }
+
+    [Fact]
+    public void RemovePassengerCommand_RemovesPassengerWhenMultipleExist()
+    {
+        var passenger1 = new PassengerFormViewModel();
+        var passenger2 = new PassengerFormViewModel();
+        _viewModel.Passengers.Clear();
+        _viewModel.Passengers.Add(passenger1);
+        _viewModel.Passengers.Add(passenger2);
+
+        _viewModel.RemovePassengerCommand.Execute(passenger1);
+
+        _viewModel.Passengers.Count.Should().Be(1);
+        _viewModel.Passengers[0].Should().Be(passenger2);
+    }
+
+    [Fact]
+    public void RemovePassengerCommand_DoesNotRemoveWhenOnlyOnePassenger()
+    {
+        var passenger = new PassengerFormViewModel();
+        _viewModel.Passengers.Clear();
+        _viewModel.Passengers.Add(passenger);
+
+        _viewModel.RemovePassengerCommand.Execute(passenger);
+
+        _viewModel.Passengers.Count.Should().Be(1);
+    }
+
+    [Fact]
+    public async Task ConfirmBookingCommand_CallsServiceAndRaisesEvent()
+    {
+        var flight = new Flight { FlightId = 1, Route = new Route { Capacity = 180 } };
+        var user = new User { UserId = 1, Email = "andrei.tudor@gmail.com" };
+        
+        _mockBookingService.Setup(s => s.GetAvailableAddOnsAsync()).ReturnsAsync(new List<AddOn>());
+        _mockBookingService.Setup(s => s.GetOccupiedSeatsAsync(It.IsAny<int>())).ReturnsAsync(new List<string>());
+        _mockBookingService.Setup(s => s.CalculateMaxPassengers(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>())).Returns(5);
+        _mockBookingService.Setup(s => s.CreateTickets(It.IsAny<Flight>(), It.IsAny<User>(), It.IsAny<List<PassengerData>>(), It.IsAny<float>()))
+            .Returns(new List<Ticket> { new Ticket() });
+        _mockBookingService.Setup(s => s.SaveTicketsAsync(It.IsAny<List<Ticket>>())).ReturnsAsync(true);
+        _mockBookingService.Setup(s => s.ValidatePassengers(It.IsAny<List<PassengerData>>())).Returns("");
+        _mockPricingService.Setup(s => s.CalculatePriceBreakdown(It.IsAny<Flight>(), It.IsAny<User>(), It.IsAny<List<Ticket>>()))
+            .Returns(new PriceBreakdown { FinalTotal = 100 });
+
+        await _viewModel.InitializeAsync(flight, user, 1);
+        
+        var passenger = _viewModel.Passengers[0];
+        passenger.FirstName = "Andrei";
+        passenger.LastName = "Tudor";
+        passenger.Email = "andrei@gmail.com";
+        passenger.SelectedSeat = "1A";
+
+        var bookingConfirmedRaised = false;
+        _viewModel.BookingConfirmed += (s, e) => bookingConfirmedRaised = true;
+
+        _viewModel.ConfirmBookingCommand.Execute(null);
+
+        int retries = 10;
+        while (!bookingConfirmedRaised && retries > 0)
+        {
+            await Task.Delay(50);
+            retries--;
+        }
+
+        _mockBookingService.Verify(s => s.SaveTicketsAsync(It.IsAny<List<Ticket>>()), Times.Once);
+        bookingConfirmedRaised.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task OnNavigatedToAsync_RedirectsToAuthWhenNotAuthenticated()
+    {
+        UserSession.CurrentUser = null;
+        var flight = new Flight { FlightId = 1, Route = new Route() };
+
+        await _viewModel.OnNavigatedToAsync(new object[] { flight });
+
+        _mockNavigationService.Verify(n => n.NavigateTo(typeof(View.AuthPage), null), Times.Once);
+    }
+
+    [Fact]
+    public void OnNavigatedToAsync_ReturnsFalseWhenNoFlight()
+    {
+        var result = _viewModel.OnNavigatedToAsync(new object[] { null }).Result;
+
+        result.Should().BeFalse();
+    }
+}
+
